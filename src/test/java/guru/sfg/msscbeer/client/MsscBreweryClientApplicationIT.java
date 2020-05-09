5eae9e0e3075a5d39810ff49591fe7c0a879cf81
yul <yu.li@tecsys.com>
2020-05-09 16:03:32 -0400

    Creating REST Client for Customer Service. Closes #6

--- src/test/java/guru/sfg/msscbeer/client/MsscBreweryClientApplicationIT.java
+++ src/test/java/guru/sfg/msscbeer/client/MsscBreweryClientApplicationIT.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MsscBreweryClientApplicationIT {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }///:~
